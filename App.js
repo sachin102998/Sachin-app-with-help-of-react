1f061432d0a138b41fa024371d8005a355dd6e0e
sachin102998 <64820972+sachin102998@users.noreply.github.com>
2022-03-18 11:33:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+    <p>{5*5}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sachin-App
+          
+        </a>
+      </header>
+    </div>
+    </React.Fragment>/*we can use react.fragement to make two parent classes inside the react component*/
+  );
+}
+
+export default App;
